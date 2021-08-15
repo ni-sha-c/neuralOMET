802d59eace36ccdcd9383fc955f06641457a58ff
Nisha Chandramoorthy <nishac@mit.edu>
2021-08-15 19:08:29 -0400

    Add plots of lyap

--- example.jl
+++ example.jl
@@ -9,7 +9,7 @@ function d2next(w, η)
 	return -2π*2π*η*sin(2π*w)
 end
 function lyap_exp(w, η)
-	N = 10000
+	N = 100000
 	le = 0.0
 	for n = 1:N
 		le += log(abs(dnext(w, η)))/N
@@ -23,23 +23,5 @@ function run(w, η, n)
 	end
 	return w
 end
-function bifurcation()
-	npts = 500
-	wstar = rand(npts)
-	nSteps = 100000
-	η = LinRange(0.01, 1.0, 100)
-	fig, ax = subplots()
-	ax.xaxis.set_tick_params(labelsize=30)
-	ax.yaxis.set_tick_params(labelsize=30)
 
-	for (i, ηi) = enumerate(η)
-		wstar .= rand(npts)
-		for j = 1:npts
-			wstar[j] = run(wstar[j], ηi, nSteps)
-		end
-		ax.plot(ηi*ones(npts), wstar, "bo", ms=3.0)
-	end
-	ax.grid(true)
-	ax.set_xlabel(L"$ \eta $", fontsize=30)
-	ax.set_ylabel(L"$ w^* $", fontsize=30)
-end
+
---
+++ plot_things.jl
@@ -0,0 +1,45 @@
+include("example.jl")
+using Printf
+function plot_lyapunov_exponents()
+	npts = 1000
+	w = rand(npts)
+	les = rand(npts)
+	nSteps = 100000
+	η = LinRange(0.1,0.9,5) 
+	fig, ax = subplots()
+	ax.xaxis.set_tick_params(labelsize=30)
+	ax.yaxis.set_tick_params(labelsize=30)
+
+	for (i, ηi) = enumerate(η)
+		w .= rand(npts)
+		for j = 1:npts
+			les[j] = lyap_exp(w[j], ηi)
+		end
+		ax.plot(w, les, label=latexstring(L"\eta = ",@sprintf("%0.1f", ηi)))
+	end
+
+	ax.grid(true)
+	ax.set_xlabel(L"$ w $", fontsize=30)
+	ax.set_ylabel(L"$\lambda(w)$", fontsize=30)
+	ax.legend(fontsize=30)
+end
+function plot_bifurcation()
+	npts = 500
+	wstar = rand(npts)
+	nSteps = 100000
+	η = LinRange(0.01, 1.0, 100)
+	fig, ax = subplots()
+	ax.xaxis.set_tick_params(labelsize=30)
+	ax.yaxis.set_tick_params(labelsize=30)
+
+	for (i, ηi) = enumerate(η)
+		wstar .= rand(npts)
+		for j = 1:npts
+			wstar[j] = run(wstar[j], ηi, nSteps)
+		end
+		ax.plot(ηi*ones(npts), wstar, "bo", ms=3.0)
+	end
+	ax.grid(true)
+	ax.set_xlabel(L"$ \eta $", fontsize=30)
+	ax.set_ylabel(L"$ w^* $", fontsize=30)
+end
