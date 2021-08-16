a79af16e5511249c0590ddcef947416fba88de68
Nisha Chandramoorthy <nishac@mit.edu>
2021-08-16 16:49:55 -0400

    add lyap exps calculation in a separate file

---
+++ src/driver.jl
@@ -0,0 +1,17 @@
+function lyap_exp(w, η)
+	N = 100000
+	le = 0.0
+	for n = 1:N
+		le += log(abs(dnext(w, η)))/N
+		w = next(w, η)
+	end
+	return le
+end
+function run(w, η, n)
+	for i = 1:n
+		w = next(w, η)
+	end
+	return w
+end
+
+
