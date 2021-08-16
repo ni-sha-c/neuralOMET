e1a0b080f69aafc656f3205aaa493bd530cf35d7
Nisha Chandramoorthy <nishac@mit.edu>
2021-08-16 16:49:26 -0400

    Remove functions not related to map defn

--- examples/convex.jl
+++ examples/convex.jl
@@ -1,4 +1,3 @@
-using PyPlot
 function next(w, η)
 	return (w + η*sin(2π*w) + 1) % 1
 end
@@ -8,20 +7,3 @@ end
 function d2next(w, η)
 	return -2π*2π*η*sin(2π*w)
 end
-function lyap_exp(w, η)
-	N = 100000
-	le = 0.0
-	for n = 1:N
-		le += log(abs(dnext(w, η)))/N
-		w = next(w, η)
-	end
-	return le
-end
-function run(w, η, n)
-	for i = 1:n
-		w = next(w, η)
-	end
-	return w
-end
-
-
