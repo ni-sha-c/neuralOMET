64b3125a1f84bc0491f4f6ea9c3fdb34d096b207
Nisha Chandramoorthy <nishac@mit.edu>
2021-09-09 19:24:56 -0400

    manually added derivatives because Zygot was not working

--- examples/nonconvex_smooth_noncompact.jl
+++ examples/nonconvex_smooth_noncompact.jl
@@ -2,8 +2,24 @@ using Zygote
 function loss(w, s=4.0)
 	return (w[1]*w[2] - s)^2/2
 end
-grad_loss(w, s=4.0) = gradient(w -> loss(w, s), w)[1]
+function grad_loss(w, s=4.0) 
+	term = w[1]*w[2] - s
+	return term*[w[2], w[1]]
+end
+function hess_loss(w, s=4.0)
+	term = w[1]*w[2] - s
+	dterm = [w[2] w[1]]
+	return term*[0 1;1 0] .+ 
+	[w[2]*dterm; w[1]*dterm] 
+end
 next(w, η, s=4.0) = w - η*grad_loss(w, s)
-dnext(w, η, s=4.0) = jacobian(w -> next(w, η, s), w)[1]
-d2next(w, η, s=4.0) = jacobian(w -> dnext(w, η, s), w)[1]
-
+dnext(w, η, s=4.0) = I(2) - η*hess_loss(w, s)    
+function d2next(w, η, s=4.0)
+    term = w[1]*w[2] - s
+    dterm = [w[2] w[1]]
+    d2term = [0 1;1 0]
+    dfirst = [0 0;dterm; dterm; 0 0]
+    dsecond = [0 2*w[2]; w[2] w[1]; 
+		   w[2] w[1]; 2*w[1] 0]
+    return -η*(dfirst .+ dsecond)	
+end
