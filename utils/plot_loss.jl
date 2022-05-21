2158c32062a2aa68fff4e8a82bf2411db7888650
Nisha Chandramoorthy <nishac@mit.edu>
2022-05-21 17:22:06 -0400

    add filenames for saving figs.

--- utils/plot_loss.jl
+++ utils/plot_loss.jl
@@ -7,24 +7,34 @@ function plot_loss_landscape(s=1.0)
 	w = rand(npts)
 	lw = rand(npts)
 	fig, ax = subplots()
-	ax.xaxis.set_tick_params(labelsize=30)
-	ax.yaxis.set_tick_params(labelsize=30)
+	ax.xaxis.set_tick_params(labelsize=16)
+	ax.yaxis.set_tick_params(labelsize=16)
 	for (i, wi) = enumerate(w)
        lw[i] = loss(wi,s) 
 	end
 	ax.plot(w, lw,".")
+	ax.grid(true)
+	ax.set_xlabel("w", fontsize=16)
+	ax.set_ylabel("loss", fontsize=16)
+
+	fig.savefig(string("../plots/lossToy", string(s), ".png"))
 end
 function plot_sharpness(η, s=1.0)
 	npts = 50000
 	w = rand(npts)
 	lw = rand(npts)
 	fig, ax = subplots()
-	ax.xaxis.set_tick_params(labelsize=30)
-	ax.yaxis.set_tick_params(labelsize=30)
+	ax.xaxis.set_tick_params(labelsize=16)
+	ax.yaxis.set_tick_params(labelsize=16)
 	for (i, wi) = enumerate(w)
 		lw[i] = abs(dnext(wi, η, s) - 1.0)/η
 	end
 	ax.plot(w, lw,".")
+	ax.grid(true)
+	ax.set_xlabel("w", fontsize=16)
+	ax.set_ylabel("sharpness", fontsize=16)
+
+	fig.savefig(string("../plots/sharpnessToy", string(s), ".png"))
 end
 
 function plot_orbits(s)
@@ -42,11 +52,12 @@ function plot_orbits(s)
 		for j = 1:npts
 			wstar[j] = run(wstar[j], ηi, nSteps, s)
 		end
-		ax.plot(ηi*ones(npts), wstar, "bP", ms=3.0)
+		ax.plot(ηi*ones(npts), wstar, "bP", ms=5.0)
 	end
 	ax.grid(true)
 	ax.set_xlabel(L"$ \eta $", fontsize=16)
-	ax.set_ylabel(L"$ w^* $", fontsize=16)
+	ax.set_ylabel("attractor", fontsize=16)
+	fig.savefig(string("../plots/attractorToy", string(s), ".png"))
 end
 function get_lyaps(η, s, npts)
 	w = rand(npts)
@@ -61,16 +72,14 @@ function plot_lyaps(s)
 	fig, ax = subplots()
 	ax.xaxis.set_tick_params(labelsize=16)
 	ax.yaxis.set_tick_params(labelsize=16)
-	npts = 100
+	npts = 10
 	for (i, ηi) = enumerate(η)
 		@show i, ηi
 		lw = get_lyaps(ηi, s, npts)
-		ax.plot(ηi*ones(npts), lw, "rv", ms=3.0)
+		ax.plot(ηi*ones(npts), lw, "kv", ms=5.0)
 	end
 	ax.grid(true)
 	ax.set_xlabel(L"$ \eta $", fontsize=16)
 	ax.set_ylabel(L"$ Lyapunov exponent $", fontsize=16)
-
-
-
+	fig.savefig(string("../plots/LEsToy", string(s), ".png"))
 end
